9d9e43bc314e9fc38e63db8d395c71f7cbbabf22
Artem <mikstrip@yandex.ru>
2021-12-19 01:41:06 +0300

    added View Binding

---
+++ app/src/main/java/com/artem/noteskotlin/ListFragment.kt
@@ -0,0 +1,39 @@
+package com.artem.noteskotlin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.artem.noteskotlin.databinding.FragmentListBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+
+class ListFragment : Fragment() {
+
+    private lateinit var binding: FragmentListBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding= FragmentListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            ListFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
